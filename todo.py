d885a28336d1f9c8f5c97be9b7c854d72d144d0e
Drobyshev <drobyshev_dima2008@mail.ru>
2026-02-02 09:44:09 +0500

    Добавленна функция добавления задач

--- todo.py
+++ todo.py
@@ -1,1 +1,27 @@
-qwerty
+tasks = []
+
+def add_task(text):
+    tasks.append(text)
+    print(f"Добавлено: {text}")
+
+# Тест функции
+add_task("Изучить Git")
+
+def show_tasks():
+    print("\nВаш список дел:")
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+
+# Тест
+show_tasks()
+
+def delete_task(number):
+    if 0 < number <= len(tasks):
+        removed = tasks.pop(number - 1)
+        print(f"Удалено: {removed}")
+    else:
+        print("Ошибка: задачи с таким номером нет.")
+
+# Тест
+delete_task(1)
+show_tasks()
